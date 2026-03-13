4e89cfc3aa83d4546a26adc8306ed05e037aa210
Stevie Larsen <maxpendragon@gmail.com>
2026-03-12 17:40:04 -0700

    Got animation tree working for walking and idle animations

--- Code/player.gd
+++ Code/player.gd
@@ -2,7 +2,7 @@ class_name Player
 extends CharacterBody3D
 
 @export var player_id:int = 0
-@export var chosen_character := Enums.PlayerCharacter.MAGURO
+@export var chosen_character := Enums.PlayerCharacter.SYLKIE
 @export var using_device: int = -1
 @export var is_AI:bool = false
 
@@ -25,9 +25,12 @@ signal player_just_scored
 func _ready() -> void:
 	$InteractingComponent.player_ref = self
 	original_location = global_position
+	animation_tree.active = true
 	#var device: int = PlayerManager.get_player_device(player_id)
 	#input = DeviceInput.new(device)
 
+func _process(delta: float) -> void:
+	update_animation_parameters()
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
@@ -72,6 +75,10 @@ func update_animation_parameters() -> void:
 		animation_tree["parameters/conditions/is_walking"] = false
 		animation_tree["parameters/conditions/is_idle"] = true
 	else:
+		if velocity.x < 0:
+			flip_sprites(-1)
+		else:
+			flip_sprites(1)
 		animation_tree["parameters/conditions/is_walking"] = true
 		animation_tree["parameters/conditions/is_idle"] = false
 		animation_tree["parameters/conditions/cuts"] = false
@@ -83,6 +90,15 @@ func update_animation_parameters() -> void:
 	else:
 		animation_tree["parameters/conditions/grabs"] = false
 		animation_tree["parameters/conditions/drops_item"] = true
+		
+func flip_sprites(blend_position: float) -> void:
+	animation_tree.set("parameters/Anticipslash/blend_position", blend_position)
+	animation_tree.set("parameters/Carry/blend_position", blend_position)
+	animation_tree.set("parameters/Grab/blend_position", blend_position)
+	animation_tree.set("parameters/Hold/blend_position", blend_position)
+	animation_tree.set("parameters/Idle/blend_position", blend_position)
+	animation_tree.set("parameters/Slash/blend_position", blend_position)
+	animation_tree.set("parameters/Walk/blend_position", blend_position)
 
 func _on_scoring_calculator_finished_cutting(score: int, mult: bool) -> void:
 	is_cutting = false
