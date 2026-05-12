2a9ae9cd9e8ea64a4202deeb04abb50f67018386
Stevie Larsen <maxpendragon@gmail.com>
2026-05-11 17:41:45 -0700

    Corrections to Base Cursor script and scene

--- Code/Character Select/base_cursor.gd
+++ Code/Character Select/base_cursor.gd
@@ -8,8 +8,9 @@ extends Sprite2D
 var controllerID : int
 var token : PlayerToken
 
+
 func _ready() -> void:
-	var newToken = preload("res://Scenes/Components/Character Select/BaseToken.tscn")
+	var newToken = preload("res://Scenes/Components/Character Select/BaseBadge.tscn")
 	token = newToken.instantiate()
 	add_child(token)
 	token.position = token_coord.position
@@ -19,3 +20,9 @@ func _ready() -> void:
 func _process(_delta: float) -> void:
 	var move : Vector2 = Input.get_vector("move_left", "move_right","move_up","move_down")
 	position = position + move * cursor_speed
+
+
+#func set_player_color() -> void:
+	#We need this function to set the Material colors for the Cursor Sprite2D node and the Badge Sprite2D Node
+	#We also need it to change the texture of the BadgeLabel Sprite2D node depending on what the playerId is
+	
