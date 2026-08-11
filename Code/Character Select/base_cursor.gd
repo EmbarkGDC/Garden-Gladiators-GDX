d2b91769e51ceae457269f48da1007e9add3cb6c
Stevie Larsen <maxpendragon@gmail.com>
2026-08-10 17:33:17 -0700

    Added multiplayer updates to main

--- Code/Character Select/base_cursor.gd
+++ Code/Character Select/base_cursor.gd
@@ -1,21 +1,57 @@
 class_name Cursor
 extends Sprite2D
 
-@onready var token_coord: Node2D = $TokenCoord
+signal chosen
+signal unchosen
+signal PlayerDrop(Self:Cursor)
+
+@onready var badge_coord: Node2D = $BadgeCoord
 
 @export var cursor_speed: float
 
-var controllerID : int
-var token : PlayerToken
+var controllerID: int:
+	set(ID):
+		controllerID = ID
+		if badge != null:
+			badge.controllerID = controllerID
+var badge: PlayerBadge
+var currentHoverPortrait: Portrait
+var hasChosen: bool= false
+var PlayerColor: Color = Color(0.0, 0.0, 0.0, 1.0)
+var overdropbutton: bool = false
 
 func _ready() -> void:
-	var newToken = preload("res://Scenes/Components/Character Select/BaseToken.tscn")
-	token = newToken.instantiate()
-	add_child(token)
-	token.position = token_coord.position
-	if controllerID != null:
-		token.controllerID = controllerID
+	var newbadge: Resource = preload("res://Scenes/Components/Character Select/BaseBadge.tscn")
+	badge = newbadge.instantiate()
+	add_child(badge)
+	badge.position = badge_coord.position
 
 func _process(_delta: float) -> void:
-	var move : Vector2 = Input.get_vector("move_left", "move_right","move_up","move_down")
+	var move: Vector2 = MultiplayerInput.get_vector(controllerID, "move_left", "move_right","move_up","move_down")
 	position = position + move * cursor_speed
+
+func _input(event: InputEvent) -> void:
+	print(event.device)
+	var valid: bool = false
+	if event.device == controllerID:
+		valid = true
+	if event is InputEventKey and controllerID == -1:
+		valid = true
+	if valid:
+		if event.is_action_pressed("ui_accept"):
+			#Determine whether the cursor is hover over a portrait and hasen't droppedit's token yet
+			if currentHoverPortrait != null and !hasChosen:
+				badge.reparent(currentHoverPortrait)
+				badge.getCharacter()
+				hasChosen = true
+				chosen.emit()
+			elif overdropbutton:
+				PlayerDrop.emit(self)
+		elif event.is_action_pressed("ui_cancel"):
+			#determines whether the token is dropped on a portrait
+			if hasChosen:
+				badge.remove()
+				badge.reparent(self)
+				badge.position = badge_coord.position
+				hasChosen = false
+				unchosen.emit()
---
+++ Code/Character Select/character_select_main.gd
@@ -0,0 +1,87 @@
+class_name CharacterSelect
+extends Control
+
+signal player_joined
+signal character_chosen
+signal character_unchosen
+signal player_dropout
+signal all_players_ready
+ 
+@onready var cursor_spawn: Node2D = $CursorSpawn
+@onready var player_colors : PackedColorArray = Global.PlayerUIColors
+
+@export var DeviceAssign: DeviceAssign
+@export var packed_cursor : PackedScene
+
+var cursors : Array[Cursor]
+var PlayersReady: bool = false
+var ReadyPlayers: int = 0
+var playerdropped: bool = false
+
+func _input(event: InputEvent) -> void:
+	if playerdropped:
+		playerdropped = false
+		return
+	var device:int
+	if event is InputEventMouseMotion:
+		return
+	if event is InputEventKey:
+		device = -1
+	else:
+		device = event.device
+	var same : bool = false
+	var num : int = 0
+	while !same and num < DeviceAssign.using_device.size():
+		if device == DeviceAssign.using_device[num]:
+			same = true
+		else:
+			num += 1
+	if same:
+		var search: Cursor = find_cursor(device)
+		if search == null:
+			var newCursor : Cursor
+			newCursor = packed_cursor.instantiate()
+			newCursor.controllerID = device
+			newCursor.chosen.connect(new_ready_player)
+			newCursor.chosen.connect(character_chosen.emit)
+			newCursor.unchosen.connect(not_ready_player)
+			newCursor.unchosen.connect(character_unchosen.emit)
+			newCursor.PlayerDrop.connect(remove_cursor)
+			cursors.append(newCursor)
+			newCursor.PlayerColor = Global.PlayerUIColors[num]
+			add_child(newCursor)
+			newCursor.position = cursor_spawn.position
+			player_joined.emit()
+		else:
+			if device == 0:
+				if PlayersReady and event.is_action_pressed("ui_accept"):
+					all_players_ready.emit()
+
+func find_cursor(id: int) -> Cursor:
+	for i:int in cursors.size():
+		var cursor: Cursor = cursors[i]
+		if cursor.controllerID == id:
+			return cursor
+	return null
+
+func new_ready_player() -> void:
+	ReadyPlayers += 1
+	if ReadyPlayers == cursors.size():
+		PlayersReady = true
+
+func not_ready_player() -> void:
+	ReadyPlayers -= 1
+	if PlayersReady:
+		PlayersReady = false
+
+func remove_cursor(cursor:Cursor) -> void:
+	if cursor.get_child_count() == 0:
+		cursor.add_child(cursor.badge)
+	
+	DeviceAssign.player_dropout(cursor.controllerID)
+	cursors.erase(cursor)
+	if cursor.hasChosen:
+		not_ready_player()
+	cursor.queue_free()
+	player_dropout.emit()
+	playerdropped = true
