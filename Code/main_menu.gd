41e93d17542273217076d03aebe91101e3db64a7
LambentKnight <134893526+LambentKnight@users.noreply.github.com>
2026-08-17 15:19:52 -0400

    Update to Main

--- Code/main_menu.gd
+++ Code/main_menu.gd
@@ -23,7 +23,7 @@ func _on_exit_pressed() -> void:
 
 func _on_minigame_pressed() -> void:
 	print("Sushido")
-	get_tree().change_scene_to_file("res://Scenes/minigames.tscn")
+	get_tree().change_scene_to_file("res://Scenes/player_select.tscn")
 
 
 func _on_credit_pressed() -> void:
