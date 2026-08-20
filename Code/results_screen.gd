1b1b5479678f2b8c9ea08a1f710fc053bd1e9bb4
LambentKnight <134893526+LambentKnight@users.noreply.github.com>
2026-08-19 20:32:20 -0400

    Added Buttons to Results Screen

--- Code/results_screen.gd
+++ Code/results_screen.gd
@@ -4,6 +4,9 @@ extends Control
 @onready var p_2: HBoxContainer = $ResultPanel/MarginContainer/VBoxContainer/P2
 @onready var p_3: HBoxContainer = $ResultPanel/MarginContainer/VBoxContainer/P3
 @onready var p_4: HBoxContainer = $ResultPanel/MarginContainer/VBoxContainer/P4
+@onready var play_again: TextureButton = $"ResultPanel/HBoxContainer/Play Again"
+@onready var new_characters: TextureButton = $"ResultPanel/HBoxContainer/New Characters"
+@onready var main_menu: TextureButton = $"ResultPanel/HBoxContainer/Main Menu"
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -15,7 +18,20 @@ func _ready() -> void:
 	p_3.result()
 	p_4.playerN = 3
 	p_4.result()
+	
+	play_again.change_text()
+	new_characters.change_text()
+	main_menu.change_text()
+	
+	#makes it so controler input is abel to be read
+	new_characters.grab_focus()
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+#When Buttons are Pressed
+func _on_play_again_pressed() -> void:
+	get_tree().change_scene_to_file("res://Scenes/multiplayer_sushido.tscn")
+
+func _on_new_characters_pressed() -> void:
+	get_tree().change_scene_to_file("res://Scenes/Components/Character Select/character_select_main.tscn")
+
+func _on_main_menu_pressed() -> void:
+	get_tree().change_scene_to_file("res://Scenes/main_menu.tscn")
