8867c950db9cc6316dcece1b2f936347c38f3981
LambentKnight <134893526+LambentKnight@users.noreply.github.com>
2026-08-21 14:16:29 -0400

    added portraits and silloutetts to char select

--- Code/Character Select/base_portrait.gd
+++ Code/Character Select/base_portrait.gd
@@ -3,13 +3,17 @@ extends MarginContainer
 
 @onready var color_tex: TextureRect = $ColorTex
 @export var CharacterID: Util.PlayerCharacter
+@onready var char_tex: TextureRect = $CharTex
+@export var bug_tex: CompressedTexture2D
+
 var currentController: int
 var isChosen := false
 var current_cursors: Array[Cursor]
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	change_portrait()
+	add_sillouette()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -38,5 +42,14 @@ func _on_portrait_collision_area_exited(area: Area2D) -> void:
 			var priorityCursor : Cursor = current_cursors[0]
 			priorityCursor.currentHoverPortrait = self
 			color_tex.modulate = priorityCursor.PlayerColor
+			remove_sillouette()
 		else :
 			color_tex.modulate = Color(1.0, 1.0, 1.0, 1.0)
+
+func change_portrait() -> void:
+	char_tex.texture = bug_tex
+
+func add_sillouette() -> void:
+	char_tex.modulate = Color(0.09, 0.06, 0.07, 1.0)
+func remove_sillouette() -> void:
+	char_tex.modulate = Color(1.0, 1.0, 1.0, 1.0)
