c6eee09044335367d82bc97e9af3aeffdce46673
RyanOldis <156034710+RyanOldis@users.noreply.github.com>
2026-08-25 23:15:11 -0700

    Device assign rearranging

--- Code/Character Select/character_select_main.gd
+++ Code/Character Select/character_select_main.gd
@@ -3,39 +3,17 @@ extends Control
 
 signal all_players_ready
 
-@onready var cursor_spawn: Node2D = $CursorSpawn
-
-@export var packed_cursor: PackedScene
-#@export var device_assign: DeviceAssign
 @export var StartBanner: Control
-@export var cursor_materials: Array[Material]
 
-var cursors : Array[Cursor]
 var active_players: Array[bool]
 var ready_players: Array[bool]
 var characters: Array[Util.PlayerCharacter]
-# Buffer for when players dropout
-var device_just_dropped: int
-var drop_time_elapsed: float = 0
-var drop_time: float = 0.5
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	active_players = [false, false, false, false]
 	ready_players = [false, false, false, false]
 	characters = [Util.PlayerCharacter.NONE, Util.PlayerCharacter.NONE, Util.PlayerCharacter.NONE, Util.PlayerCharacter.NONE]
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	drop_time_elapsed -= delta
-
-func find_cursor(id: int) -> Cursor:
-	for i:int in cursors.size():
-		var cursor: Cursor = cursors[i]
-		if cursor.controllerID == id:
-			return cursor
-	return null
 
 func ready_check() -> bool:
 	var players_are_ready: bool = true
@@ -61,50 +39,6 @@ func character_unchosen(player: int) -> void:
 	if $StartBanner.visible:
 		$StartBanner.visible = false
 
-func remove_cursor(cursor:Cursor) -> void:
-	if cursor.get_child_count() == 0:
-		cursor.add_child(cursor.badge)
-	
-	#$DeviceAssign.player_dropout(cursor.player_slot)
-	cursors.erase(cursor)
-	cursor.queue_free()
-	#player_dropout.emit()
-
-func _on_device_assign_send_device(device: int, playernum: int) -> void:
-	# Check that we're not re-adding a player that just dropped out
-	if drop_time_elapsed > 0 and device_just_dropped == device:
-		return
-	
-	active_players[playernum] = true
-	var newCursor : Cursor = packed_cursor.instantiate()
-	newCursor.controllerID = device
-	newCursor.player_slot = playernum
-	
-	# connect signals
-	newCursor.chosen.connect(Callable(self, "character_chosen"))
-	newCursor.unchosen.connect(Callable(self, "character_unchosen"))
-	newCursor.player_drop.connect(remove_cursor)
-	newCursor.player_drop.connect($DeviceAssign.player_dropout)
-	newCursor.start_pressed.connect(start_game)
-	
-	cursors.append(newCursor)
-	newCursor.PlayerColor = Global.PlayerUIColors[playernum]
-	newCursor.material_to_set = cursor_materials[playernum]
-	newCursor.position = cursor_spawn.position
-	#player_joined.emit()
-	call_deferred("add_child", newCursor)
-
-func _on_device_assign_drop_player(player: int) -> void:
-	var dropped: Cursor = find_cursor(player)
-	device_just_dropped = dropped.controllerID
-	drop_time_elapsed = drop_time
-	print(dropped.controllerID)
-	
-	remove_cursor(dropped)
-	active_players[player] = false
-	ready_players[player] = false
-	characters[player] = Util.PlayerCharacter.NONE
-
 func start_game() -> void:
 	if ready_check():
 		PlayerSelectBridge.player_using_device = $DeviceAssign.using_device
@@ -114,3 +48,26 @@ func start_game() -> void:
 		
 		# Switch scenes
 		get_tree().change_scene_to_file("res://Scenes/multiplayer_sushido.tscn")
+
+
+func _on_device_assign_send_device(playernum: int) -> void:
+	active_players[playernum] = true
+	if !ready_check():
+		$StartBanner.visible = false
+
+func _on_device_assign_drop_player(player: int) -> void:
+	active_players[player] = false
+	ready_players[player] = false
+	characters[player] = Util.PlayerCharacter.NONE
+
+
+func _on_device_assign_start_game_input() -> void:
+	start_game()
+
+
+func _on_device_assign_character_chosen_input(player: int, character: Util.PlayerCharacter) -> void:
+	character_chosen(player, character)
+
+
+func _on_device_assign_character_unchosen_input(player: int) -> void:
+	character_unchosen(player)
--- Code/Character Select/device_assign.gd
+++ Code/Character Select/device_assign.gd
@@ -1,8 +1,22 @@
 class_name DeviceAssign
 extends Node
 
-signal send_device(device: int, playernum: int)
+signal send_device(playernum: int)
 signal drop_player(player: int)
+signal start_game_input
+signal character_chosen_input
+signal character_unchosen_input
+
+@export var cursor_spawn: Node2D
+
+@export var packed_cursor: PackedScene
+@export var cursor_materials: Array[Material]
+
+var cursors : Array[Cursor]
+# Buffer for when players dropout
+var device_just_dropped: int
+var drop_time_elapsed: float = 0
+var drop_time: float = 0.5
 
 var using_device: Array[int]
 
@@ -11,6 +25,9 @@ func _ready() -> void:
 	# -1 is keyboard, 0+ are controllers
 	# anything less than -1 is considered unassigned
 	using_device = [-128, -128, -128, -128]
+
+func _process(delta: float) -> void:
+	drop_time_elapsed -= delta
 
 func _input(event: InputEvent) -> void:
 	# don't look for mouse input
@@ -25,6 +42,9 @@ func _input(event: InputEvent) -> void:
 		device = -1
 	else:
 		device = event.device
+	# Check that we're not re-adding a player that just dropped out
+	if drop_time_elapsed > 0 and device_just_dropped == device:
+		return
 	
 	for i:int in using_device.size():
 		# first check if this device has already been assigned
@@ -33,11 +53,59 @@ func _input(event: InputEvent) -> void:
 		# otherwise, assign device in the first availible player slot
 		elif using_device[i] < -1:
 			using_device[i] = device
-			send_device.emit(device, i)
+			add_cursor(device, i)
+			send_device.emit(i)
 			get_viewport().set_input_as_handled()
 			return
 
+func find_cursor(id: int) -> Cursor:
+	for i:int in cursors.size():
+		var cursor: Cursor = cursors[i]
+		if cursor.controllerID == id:
+			return cursor
+	return null
+
+func remove_cursor(cursor:Cursor) -> void:
+	if cursor.get_child_count() == 0:
+		cursor.add_child(cursor.badge)
+	
+	player_dropout(cursor)
+	cursors.erase(cursor)
+	cursor.queue_free()
+	#player_dropout.emit()
+
+func add_cursor(device: int, playernum: int) -> void:
+	var newCursor : Cursor = packed_cursor.instantiate()
+	newCursor.controllerID = device
+	newCursor.player_slot = playernum
+	
+	# connect signals
+	newCursor.chosen.connect(Callable(self, "character_chosen"))
+	newCursor.unchosen.connect(Callable(self, "character_unchosen"))
+	newCursor.player_drop.connect(remove_cursor)
+	newCursor.start_pressed.connect(start_game)
+	
+	cursors.append(newCursor)
+	newCursor.PlayerColor = Global.PlayerUIColors[playernum]
+	newCursor.material_to_set = cursor_materials[playernum]
+	newCursor.position = cursor_spawn.position
+	#player_joined.emit()
+	call_deferred("add_child", newCursor)
+
 func player_dropout(player: Cursor) -> void:
+	device_just_dropped = player.controllerID
+	drop_time_elapsed = drop_time
+	print(player.controllerID)
+	
 	using_device[player.player_slot] = -128
 	print(using_device)
-	#drop_player.emit(player)
+	drop_player.emit(player.player_slot)
+
+func character_chosen(player: int, character: Util.PlayerCharacter) -> void:
+	character_chosen_input.emit(player, character)
+
+func character_unchosen(player: int) -> void:
+	character_unchosen_input.emit(player)
+
+func start_game() -> void:
+	start_game_input.emit()
